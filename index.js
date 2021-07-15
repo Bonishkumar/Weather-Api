bc2e2490e3888279c0c9950b589ecc613579e49b
Bonishkumar Kshetrimayum <kshbonish@gmail.com>
2021-07-15 20:25:29 +0530

    Output style change

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function check()
 {
-
-    if(document.getElementById("weather").value=="")
-    {
+    var city = document.getElementById("weather").value;
+    console.log(city);
+    if(city==""){
         alert("Enter a city,country or place!!!");
-  
     }
     else{
-
-        var city=document.getElementById("weather").value;
         var api=" https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=bf6935d399ffacfa4735e2e52e5e4756&units=metric";
         var ajaxrequest= new XMLHttpRequest();
    
@@ -23,10 +20,10 @@ function check()
          
              if(finaldata.cod==404){
                 document.getElementById("resultdisplay").innerHTML="CITY NOT FOUND!!!!!"
-                document.getElementById("result1").innerHTML="Temperature  :  ";
-                document.getElementById("result2").innerHTML="Feels like :  ";
-                document.getElementById("result3").innerHTML="Humidity :  ";
-                document.getElementById("result4").innerHTML="Pressure:  ";
+                document.getElementById("result1").innerHTML="NULL";
+                document.getElementById("result2").innerHTML="NULL";
+                document.getElementById("result3").innerHTML="NULL";
+                document.getElementById("result4").innerHTML="NULL";
              }
              else{
                 if(finaldata.name==undefined){
@@ -36,21 +33,22 @@ function check()
                     document.getElementById("resultdisplay").innerHTML=finaldata.name;
                 }
                 
-                document.getElementById("result1").innerHTML="Temperature  :  "+finaldata.main.temp_max+"<sup>0</sup>"+"C";
-                document.getElementById("result2").innerHTML="Feels like :  "+finaldata.main.feels_like+"<sup>0</sup>"+"C";
-                document.getElementById("result3").innerHTML="Humidity :  "+finaldata.main.humidity+"%";
-                document.getElementById("result4").innerHTML="Pressure:  "+finaldata.main.pressure;
+                document.getElementById("result1").innerHTML=finaldata.main.temp_max+"<span>&#8451;</span>";
+                document.getElementById("result2").innerHTML=+finaldata.main.feels_like+"<span>&#8451;</span>";
+                document.getElementById("result3").innerHTML=+finaldata.main.humidity+"%";
+                document.getElementById("result4").innerHTML=+finaldata.main.pressure;
             }
              }
+
             
     }
    
 
     ajaxrequest.open("Get",api,true);
     ajaxrequest.send();
-    }
-   
-
-    
 }
+
+}
+    
+
     
